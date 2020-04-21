1d993732be8e18dd960cd2d47eb6412b15771bc5
Shinoda492039 <a99yf47.12x@gmail.com>
2020-04-21 15:27:11 +0900

    Add: enable to display the acquired post data
    To automatically display the latest message.

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,31 @@
 $(function(){
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML= '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.messages').append(insertHTML);
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fall(function() {
+      alert('error');
+    });
+  };
+
   function buildHTML(message){
     if (message.image) {
-      var html = `<div class="message">
+      var html = `<div class="message" data-message-id=${message.id}>
                             <div class="upper-info">
                               <p class="upper-info__talker">
                               ${message.user_name}
@@ -16,7 +40,7 @@ $(function(){
                             <img class="lower-info__image" src=${message.image}>
                           </div>`
     } else {
-      var html = `<div class="message">
+      var html = `<div class="message" data-message-id=${message.id}>
                             <div class="upper-info">
                               <p class="upper-info__talker">
                               ${message.user_name}
@@ -58,4 +82,7 @@ $(function(){
       $('.submit-btn').prop('disabled', false);
   });
   });
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
