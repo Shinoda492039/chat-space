eb136b66e4b14996cf368d37acfc9737292aa007
Shinoda492039 <a99yf47.12x@gmail.com>
2020-05-26 09:48:25 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-<<<<<<< Updated upstream
     resources :messages, only: [:index, :create]
     namespace :api do
       resources :messages, only: :index, defaults: { format: 'json' }
     end
-=======
-    resources :messages, only: [:create]
->>>>>>> Stashed changes
   end
 end
